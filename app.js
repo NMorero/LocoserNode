5e11961480d7329ffc8797a547777bed8500fd19
Nicolas <nmorero00@gmail.com>
2020-09-25 16:47:32 -0300

    s

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {  
     if (err) throw err;  
     console.log("Connected!");  
-    connect.query("CREATE DATABASE node", function (err, result) {  
+    connection.query("CREATE DATABASE node", function (err, result) {  
     if (err) throw err;  
     console.log("Database created");  
     });  
