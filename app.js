088bf522cb6118ea87bc20aabaaa051a776b7814
Nicolas <nmorero00@gmail.com>
2020-09-26 12:31:30 -0300

    V2

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ connection.connect(function(err) {
 
 */
 
-app.use("/public", express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
